e2c5f674e1905e40b5be2979ee216be189e36f9d
Sufiyaan Tamboli <sufiyaantamboli1983@gmail.com>
2025-09-14 20:37:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
     //Write your code here
-  
+  // Get reference to the table by id
+    var table = document.getElementById("sampleTable");
+    
+    // Create a new row element
+    var newRow = table.insertRow(0); // Insert at index 0 (top)
+    
+    // Create and insert the first cell
+    var cell1 = newRow.insertCell(0);
+    cell1.innerHTML = "New Cell1";
+    
+    // Create and insert the second cell
+    var cell2 = newRow.insertCell(1);
+    cell2.innerHTML = "New Cell2";
   
 }
